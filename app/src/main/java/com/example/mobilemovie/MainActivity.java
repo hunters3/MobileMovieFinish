de0d11d6fbec691b79cf6c7d07d3881be12ccec1
Hunter Sutherland <hunters3@vt.edu>
2021-04-16 14:48:00 -0400

    total finish

--- app/src/main/java/com/example/mobilemovie/MainActivity.java
+++ app/src/main/java/com/example/mobilemovie/MainActivity.java
@@ -1,14 +1,65 @@
 package com.example.mobilemovie;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.codepath.asynchttpclient.AsyncHttpClient;
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
+import com.example.mobilemovie.adapters.MovieAdapter;
+import com.example.mobilemovie.models.Movie;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import okhttp3.Headers;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String url = "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
+    public static final String tag = "MainActivity";
+
+    List<Movie> movies;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        RecyclerView rvMovies = findViewById(R.id.rvMovies);
+        movies = new ArrayList<>();
+
+        MovieAdapter movieAdapter = new MovieAdapter(this, movies);
+
+        rvMovies.setAdapter(movieAdapter);
+
+        rvMovies.setLayoutManager(new LinearLayoutManager(this));
+
+        AsyncHttpClient newClient = new AsyncHttpClient();
+        newClient.get(url, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Headers headers, JSON json) {
+                Log.d(tag, "onSuccess");
+                JSONObject jObject = json.jsonObject;
+                try {
+                    JSONArray results = jObject.getJSONArray("results");
+                    Log.i(tag, "Results: " + results.toString());
+                    movies.addAll(Movie.fromJsonArray(results));
+                    movieAdapter.notifyDataSetChanged();
+                } catch (JSONException e) {
+                    Log.d(tag, "Hit json exception", e);
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
+                Log.d(tag, "onFailure");
+            }
+        });
     }
 }
